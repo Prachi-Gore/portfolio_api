16228d9e75300b7142a8430d8c9568ee487b4550
Prachi Gore <prachigore408@gmail.com>
2025-01-02 23:30:21 +0530

    log added in build file

--- build.sh
+++ build.sh
@@ -1,6 +1,12 @@
 set -o errexit # exit on error
 
+echo "Installing dependencies..."
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
+
+echo "Applying database migrations..."
 python manage.py migrate
+
+
+echo "Deployment completed successfully."
