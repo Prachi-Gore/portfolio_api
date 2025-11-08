766be4c528e28207336dee77760214e737e975b1
Prachi Gore <prachigore408@gmail.com>
2025-11-09 01:53:17 +0530

    Lists are not currently supported in HTML input.

--- skillsProjects/admin.py
+++ skillsProjects/admin.py
@@ -1,19 +1,6 @@
-# from django.contrib import admin
-# from .models import Skill,Project
-
-# # Register your models here
-# admin.site.register(Skill)
-# admin.site.register(Project)
-
 from django.contrib import admin
-from .models import Skill, Project
+from .models import Skill,Project
 
-@admin.register(Skill)
-class SkillAdmin(admin.ModelAdmin):
-    list_display = ("name", "iconUrl")
-
-
-@admin.register(Project)
-class ProjectAdmin(admin.ModelAdmin):
-    list_display = ("name", "code", "preview")
-    filter_horizontal = ("skills",)  # 👈 This enables multi-select list for skills
+# Register your models here
+admin.site.register(Skill)
+admin.site.register(Project)
